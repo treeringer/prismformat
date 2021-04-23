f62f4e35a40c9ee0351be55b055e816e43e7198d
Stockton Maxwell <52714209+treeringer@users.noreply.github.com>
2021-04-23 10:02:44 -0400

    Update format_prism.R

--- format_prism.R
+++ format_prism.R
@@ -5,15 +5,15 @@
 setwd("C:/Users/rmaxwell2/Desktop/work_r")
 #install.packages(tidyverse)
 library(tidyverse)
-tmean <- read.table(file = "wolf_tmean_prism_original.csv", skip = 10, header = TRUE, sep = ",") #skips reading the header
-head(tmean)
-year <- substr(tmean[,1], 1, 4) #pull year from date string using character positions
-month <- substr(tmean[,1], 6, 7) #pull month from date string using character positions
-tmean <- add_column(tmean, year = year, .after = 1) #add in year column
-tmean <- add_column(tmean, month = month, .after = 2) #add in month column
-tmean <- tmean[,2:4] #3 column data frame
-
-date <- tmean[1:1500,3] #change to your number of rows
+data <- read.table(file = "wolf_tmean_prism_original.csv", skip = 10, header = TRUE, sep = ",") #skips reading the header
+head(data)
+year <- substr(data[,1], 1, 4) #pull year from date string using character positions
+month <- substr(data[,1], 6, 7) #pull month from date string using character positions
+data <- add_column(data, year = year, .after = 1) #add in year column
+data <- add_column(data, month = month, .after = 2) #add in month column
+data <- data[,2:4] #3 column data frame
+nrow(data) #get number of rows or observations
+date <- data[1:1500,3] #change to your number of rows
 date.mat <- matrix(date, nrow =12) #create matrix with columns and rows
 tdate <- t(date.mat) #transpose
 year <- c(1895:2019) #create year column for your year range
@@ -36,8 +36,9 @@ write.csv(date.monthly,file = "tmean_wolf_12.csv") #right to a file for later
 setwd("C:/Users/rmaxwell2/Desktop/work_r")
 #install.packages(tidyverse)
 library(tidyverse)
-tmean <- read.table(file = "wolf_tmean_long.csv", header = TRUE, sep = ",")
-date <- tmean[1:1500,3] #change to your number of rows
+data <- read.table(file = "wolf_tmean_long.csv", header = TRUE, sep = ",")
+nrow(data) #get number of rows or observations
+date <- data[1:1500,3] #change to your number of rows
 date.mat <- matrix(date, nrow =12) #create matrix
 tdate <- t(date.mat) #transpose
 year <- c(1895:2019) #create year column for your year range
