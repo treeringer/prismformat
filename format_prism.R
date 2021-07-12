7893af79eedb4fdc483a9a53f471cd44745cc5a1
Stockton Maxwell <52714209+treeringer@users.noreply.github.com>
2021-07-12 13:46:35 -0400

    Update format_prism.R

--- format_prism.R
+++ format_prism.R
@@ -2,8 +2,9 @@
 
 #try with PRISM stacked or long format downloaded from PRISM data viewer for a single point
 #you will need to download a csv file of climate data from https://prism.oregonstate.edu/explorer/
+
 setwd("C:/Users/rmaxwell2/Desktop/work_r") #set your working directory
-#install.packages(tidyverse)
+install.packages(tidyverse)
 library(tidyverse)
 data <- read.table(file = "wolf_tmean_prism_original.csv", skip = 10, header = TRUE, sep = ",") #skips reading the header
 head(data)
@@ -12,48 +13,9 @@ month <- substr(data[,1], 6, 7) #pull month from date string using character pos
 data <- add_column(data, year = year, .after = 1) #add in year column
 data <- add_column(data, month = month, .after = 2) #add in month column
 data <- data[,2:4] #3 column data frame
-nrow(data) #get number of rows or observations
-date <- data[1:1500,3] #change to your number of rows
-date.mat <- matrix(date, nrow =12) #create matrix with columns and rows
-tdate <- t(date.mat) #transpose
-year <- c(1895:2019) #create year column for your year range
-date.monthly <- cbind(year, tdate) #combine variables into data frame
-colnames(date.monthly) <- c("year", "jan", "feb", "march", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec") #set headers
-head(date.monthly)
-write.csv(date.monthly,file = "tmean_wolf_12.csv") #write to a file for later
-
-#keep going if you want to remove the ID column in the .csv and set years as rows
-head(date.monthly) #check it out
-row.names(date.monthly) #check row names
-row.names(date.monthly) <- year #set row names as year
-date.monthly <- date.monthly[,2:13] #remove year column
-head(date.monthly) #check it out
-summary(date.monthly) #check it out
-write.csv(date.monthly,file = "tmean_wolf_12.csv") #right to a file for later
-
-#######################
-#try with data that already behave
-setwd("C:/Users/rmaxwell2/Desktop/work_r")
-#install.packages(tidyverse)
-library(tidyverse)
-data <- read.table(file = "wolf_tmean_long.csv", header = TRUE, sep = ",")
-nrow(data) #get number of rows or observations
-date <- data[1:1500,3] #change to your number of rows
-date.mat <- matrix(date, nrow =12) #create matrix
-tdate <- t(date.mat) #transpose
-year <- c(1895:2019) #create year column for your year range
-date.monthly <- cbind(year, tdate) #combine variables into data frame
-colnames(date.monthly) <- c("year", "jan", "feb", "march", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec") #set headers
-head(date.monthly)
-write.csv(date.monthly,file = "tmean_wolf_12.csv") #write to a file for later
-
-#keep going if you want to remove the ID column in the .csv and set years as rows
-head(date.monthly) #check it out
-row.names(date.monthly) #check row names
-row.names(date.monthly) <- year #set row names as year
-date.monthly <- date.monthly[,2:13] #remove year column
-head(date.monthly) #check it out
-summary(date.monthly) #check it out
-write.csv(date.monthly,file = "tmean_wolf_12.csv") #right to a file for later
-
-
+colnames(data) <- c("year","month", "climate")
+data <- as.data.frame(data)
+data$month <- as.factor(data$month)
+data$year <- as.numeric(data$year)
+x <- pivot_wider(data, names_from = month, values_from = climate)
+write.csv(x,file = "tmean_wolf_12.csv") #write to a file for later
